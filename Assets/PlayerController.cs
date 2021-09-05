2cf09fdb716f324034613aca718f744571c84249
RKSDude <anotherguywhoisbackup@gmail.com>
2021-09-05 12:48:32 +0800

    Fix player controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -22,14 +22,20 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVertical = transform.forward * zMovement;
 
         Vector3 _velocity = (moveHorizontal + moveVertical).normalized * speed;
-
         motor.Move(_velocity);
 
-        //Y Rotation (LEFT/RIGHT ROTATION NOT VERTICAL)
+        //Y Rotation (LEFT/RIGHT ROTATION NOT VERTICAL (rotation about the Y axis))
         float yRotation = Input.GetAxisRaw("Mouse X");
 
         Vector3 _rotation = new Vector3 (0f, yRotation, 0f) * sensitivity;
 
         motor.Rotate(_rotation);
+
+        //Camera Rotation (Vertical (rotation about the X axis))
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _camRotation = new Vector3 (xRotation, 0f, 0f) * sensitivity;
+
+        motor.RotateCamera(_camRotation);
     }
 }
